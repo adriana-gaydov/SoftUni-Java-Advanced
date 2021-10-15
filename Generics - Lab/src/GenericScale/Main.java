b7730a8f988eb81ec576aea53e76fd3b3282df8d
adriana-gaydova <86288896+adriana-gaydova@users.noreply.github.com>
2021-10-15 22:19:42 +0300

    Delete Generics - Lab directory

--- Generics - Lab/src/GenericScale/Main.java
+++
@@ -1,8 +0,0 @@
-package GenericScale;
-
-public class Main {
-    public static void main(String[] args) {
-        Scale<String> stringScale = new Scale<>("a", "c");
-        System.out.println(stringScale.getHeavier());
-    }
-}
--- Generics - Lab/src/ListUtilities/Main.java
+++
@@ -1,7 +0,0 @@
-package ListUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
